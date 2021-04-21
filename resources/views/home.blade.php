164e8bc4c31790832088eb695750c205c4c473b3
= <you@example.com>
2021-04-21 10:11:39 +0700

    Add Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,68 @@
+@extends('main')
+
+@section('title')
+
+@section('breadcrumbs')
+
+<div class="breadcrumbs">
+    <div class="col-sm-4">
+        <div class="page-header ">
+            <div class="page-title">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-8">
+        <div class="page-header float-right">
+            <div class="page-title">
+                <ol class="breadcrumb text-right">
+                    <li><a href="#">Dashboard</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('content')
+
+    <div class="content mt-3">
+    
+        <div class="animated fadeIn">
+        <div class="page-header float-right">
+            <div class="page-title">
+                <ol class="breadcrumb text-right">
+                </div>
+    <h3 class='post-title entry-title'>Mau Laundry tanpa perlu datang ketempat laundry? Kami solusinya!!!</h3>
+    <div class='post-header'>
+    <div class='post-header-line-1'>
+
+    <span class='byline post-author vcard'>
+    <span class='post-author-label'><i aria-hidden='true' class='fa fa-user-circle'></i>Oleh</span>
+    <span class='fn'>
+        <meta content='/home'/>
+        <a class='g-profile' href='/home' rel='author' title='author profile'>
+        <span>Rip Aim</span>
+        </a>
+    </span>
+    </span>
+    </div>
+    </div>
+        <div class='post-body entry-content float-container' id='post-body-8447892443786240306'>
+    <!--    <a href="https://1.bp.blogspot.com/-PcyaoBRvACc/X6q5lXe8CTI/AAAAAAABYw4/4ykid95fGiAwmPqhGeHvLnUXCp8xkHfCgCPcBGAYYCw/s1600/open-trip-bromo.jpg" > -->
+        <img alt="wisata bromo" border="P" src="https://i.pinimg.com/originals/b4/74/6d/b4746d7a7393feff2389ef918fca4a70.png" title="paket wisata open trip bromo sunrise new normal 2020"/></a><br />
+    <br />
+    Kami bisa menjadi opsi atas pakaian kamu yang menumpuk tapi kamu gak punya waktu untuk mencucinya, hal itu tidak menjadi alasan untuk kamu semakin pusing tujuh keliling dalam menjalani hari.<br />
+    Tidak bisa dipungkiri bahwa perkembangan teknologi semakin mumpuni di masa sekarang, khususnya di bidang laundry pakaian.<br />
+    Perkembangan tersebut menciptakan inovasi yang cocok banget untuk kamu yang males gerak alias mager dalam mencuci pakaian sendiri atau bahkan mencari jasa laundry secara langsung.<br />
+    <br />
+
+<!--    <iframe type="text/html" frameborder="0" width="900" height="600" src="https://www.youtube.com/embed/cgopt2BxGFw?autoplay=1&modestbranding=1" allowfullscreen></iframe> -->  
+    </ol>
+    </div>
+    </div>
+    </div>
+</div>
+    
+@endsection 
