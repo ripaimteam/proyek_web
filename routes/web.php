32ff945f3a08c4edc6ece44a7d43cf474b73500c
= <you@example.com>
2021-04-21 10:12:11 +0700

    Edit Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +12,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
+
+Auth::routes();
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route:: get('/home', 'HomeController@home');
+
+Route:: get('/package', 'PackageController@package');
+
+Route:: get('/contact', 'ContactController@contact');
+
+Route:: get('/about', 'AboutController@about');
